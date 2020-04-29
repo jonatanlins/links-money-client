1582f7cd26d8a372a155862130b327cd8fa9c293
Jonatan Lins <jonatanpinheiro99@gmail.com>
2020-04-29 11:46:42 -0300

    criação de página modificada

--- src/pages/CreatePage.js
+++ src/pages/CreatePage.js
@@ -10,29 +10,32 @@ import Button from "../components/Button";
 import TextInput from "../components/TextInput";
 
 function Page({ history }) {
-  const [formState, setFormState] = React.useState({});
+  const [formState, setFormState] = React.useState({ id: "forbesbr" });
+  const [pageData, setPageData] = React.useState(null);
 
   const handleInputChange = (field) => (event) => {
     const { value } = event.target;
     setFormState((state) => ({ ...state, [field]: value }));
   };
 
-  const handleSave = (event) => {
-    event.preventDefault();
-
-    const { id, name, avatar, description } = formState;
+  const handleSave = () => {
     const { user } = getUser();
 
-    const pageData = { id, name, avatar, description, owners: [user.id] };
+    const data = { ...pageData, id: formState.id, owners: [user.id] };
 
-    api.post("pages", pageData).then((response) => {
-      alert("Página criada com sucesso!");
+    api.post("pages", data).then((response) => {
+      if (response.status < 400) {
+        alert("Página criada com sucesso!");
 
-      history.push(`/pages/${response.data.id}/edit`);
+        history.push(`/pages/${response.data.id}/edit`);
+      } else {
+        alert("Ocorreu um erro, tente novamente");
+      }
     });
   };
 
-  const fetchInstagramData = () => {
+  const fetchInstagramData = (event) => {
+    event.preventDefault();
     axios
       .get(`https://www.instagram.com/${formState.id}/?__a=1`)
       .then((response) => {
@@ -40,43 +43,37 @@ function Page({ history }) {
         const description = response.data.graphql.user.biography;
         const avatar = response.data.graphql.user.profile_pic_url_hd;
 
-        setFormState((state) => ({ ...state, name, description, avatar }));
+        setPageData({ name, description, avatar });
+      })
+      .catch(() => {
+        alert("Instagram não encontrado");
       });
   };
 
   return (
     <Shell>
       <Container>
-        <form onSubmit={handleSave}>
+        <form onSubmit={fetchInstagramData}>
           <TextInput
             label="Nome de usuário"
             value={formState.id}
             onChange={handleInputChange("id")}
             required
           />
-          <Button type="button" onClick={fetchInstagramData}>
-            Pegar dados do instagram
-          </Button>
-
-          <Avatar src={formState.avatar} />
-
-          <TextInput
-            label="Nome da página"
-            value={formState.name}
-            onChange={handleInputChange("name")}
-            required
-          />
-
-          <TextInput
-            label="Biografia"
-            value={formState.description}
-            onChange={handleInputChange("description")}
-            multiline
-            required
-          />
-
-          <Button>Criar página</Button>
+          <Button>Pesquisar instagram</Button>
         </form>
+
+        {pageData && (
+          <>
+            <InstagramHeader>
+              <Avatar src={pageData.avatar} alt="" />
+              <Title>{pageData.name}</Title>
+              <Description>{pageData.description}</Description>
+            </InstagramHeader>
+
+            <Button onClick={handleSave}>Criar página</Button>
+          </>
+        )}
       </Container>
     </Shell>
   );
@@ -84,13 +81,32 @@ function Page({ history }) {
 
 const Container = styled.div``;
 
+const InstagramHeader = styled.header`
+  display: grid;
+  grid-template-columns: 5em auto;
+  grid-template-rows: 1.35em auto;
+  grid-template-areas: "avatar title actions" "avatar description actions";
+  grid-gap: 0 1em;
+  padding: 1em 0;
+`;
+
 const Avatar = styled.img`
   grid-area: avatar;
   width: 100%;
   border-radius: 9em;
   margin: auto;
-  width: 16em;
-  height: 16em;
+`;
+
+const Title = styled.h1`
+  grid-area: title;
+  margin: 0;
+  font-size: 1em;
+`;
+
+const Description = styled.p`
+  grid-area: description;
+  font-size: 0.9em;
+  margin: 0;
 `;
 
 export default Page;
