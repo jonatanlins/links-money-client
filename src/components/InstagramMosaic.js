70c621dc5d11cda0937bc47e007beaffa3e0bd5d
Jonatan Lins <jonatanpinheiro99@gmail.com>
2020-05-03 21:28:05 -0300

    instagram lauyout organizado em componentes

---
+++ src/components/InstagramMosaic.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import { FaLink } from "react-icons/fa";
+
+import InstagramMosaicButton from "./InstagramMosaicButton";
+
+function Component({ posts = [], onClick }) {
+  return (
+    <Container>
+      {posts.map((post) => (
+        <InstagramMosaicButton
+          key={post.id}
+          image={post.thumbnail}
+          onClick={() => onClick(post.link)}
+        >
+          {post.link && <FaLink />}
+        </InstagramMosaicButton>
+      ))}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: grid;
+  width: 100%;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 3px;
+`;
+export default Component;
--- src/pages/Mosaic.js
+++ src/pages/Mosaic.js
@@ -1,16 +1,18 @@
 import React from "react";
 import api from "../services/api";
 import styled, { css } from "styled-components";
-import * as FontAwesomeIcon from "react-icons/fa";
-import InstagramMosaicButton from "../components/InstagramMosaicButton";
 import YoutubeMosaicButton from "../components/YoutubeMosaicButton";
 import axios from "axios";
+
+import InstagramHeader from "../components/InstagramHeader";
+import InstagramMosaic from "../components/InstagramMosaic";
+import InstagramRoundButtons from "../components/InstagramRoundButtons";
 
 function Page({ match }) {
   const [pageData, setPageData] = React.useState(null);
   const [layout, setLayout] = React.useState("instagram");
 
-  const handleSocialButton = (buttonData) => () => {
+  const handleSocialButton = (buttonData) => {
     if (buttonData.layout) {
       setLayout(buttonData.layout);
     } else {
@@ -56,42 +58,21 @@ function Page({ match }) {
 
   return (
     <Container>
-      <Header>
-        <Avatar src={pageData.avatar} alt="" />
-        <Title>{pageData.id}</Title>
-        <Description>{pageData.description}</Description>
-      </Header>
+      <InstagramHeader
+        avatar={pageData.avatar}
+        id={pageData.id}
+        description={pageData.description}
+      />
 
-      <SocialButtonCarousel>
-        {pageData.socialButtons.map((item) => {
-          const Icon = FontAwesomeIcon[item.icon];
-
-          return (
-            <SocialButton key={item.id} onClick={handleSocialButton(item)}>
-              <SocialButtonIconWrapper background={item.gradient || item.color}>
-                <Icon />
-              </SocialButtonIconWrapper>
-              <SocialButtonLabel>{item.label}</SocialButtonLabel>
-            </SocialButton>
-          );
-        })}
-      </SocialButtonCarousel>
+      <InstagramRoundButtons
+        buttons={pageData.socialButtons}
+        onClick={handleSocialButton}
+      />
 
       {layout === "instagram" && (
         <>
           <HelpText>Toque em alguma imagem para ver mais</HelpText>
-
-          <InstagramMosaic>
-            {pageData.timeline?.map((post) => (
-              <InstagramMosaicButton
-                key={post.id}
-                image={post.thumbnail}
-                onClick={() => handleLink(post.link)}
-              >
-                {post.link && <FontAwesomeIcon.FaLink />}
-              </InstagramMosaicButton>
-            ))}
-          </InstagramMosaic>
+          <InstagramMosaic posts={pageData.timeline} onClick={handleLink} />
         </>
       )}
 
@@ -129,92 +110,6 @@ const Container = styled.div`
     `}
 `;
 
-const Header = styled.header`
-  display: grid;
-  grid-template-columns: 5em auto;
-  grid-template-rows: 1.35em auto;
-  grid-template-areas: "avatar title" "avatar description";
-  grid-gap: 0 1em;
-  margin: 1em;
-`;
-
-const Avatar = styled.img`
-  grid-area: avatar;
-  width: 100%;
-  border-radius: 9em;
-  margin: auto;
-`;
-
-const Title = styled.h1`
-  grid-area: title;
-  margin: 0;
-  font-size: 1em;
-`;
-
-const Description = styled.p`
-  grid-area: description;
-  font-size: 0.9em;
-  margin: 0;
-`;
-
-const SocialButtonCarousel = styled.div`
-  overflow-x: auto;
-  display: flex;
-  padding: 0 0.5em;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-`;
-
-const SocialButton = styled.button`
-  outline: none;
-  border: none;
-  background-color: transparent;
-  padding: 0;
-  font-size: 1em;
-  flex: 0 0 5em;
-  width: 5em;
-`;
-
-const SocialButtonIconWrapper = styled.div`
-  position: relative;
-  border-radius: 9em;
-  width: 3.5em;
-  height: 3.5em;
-  margin: 0 auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: ${(props) => props.background};
-
-  svg {
-    font-size: 1.618em;
-    color: white;
-  }
-
-  &:after {
-    content: "";
-    position: absolute;
-    height: 50px;
-    width: 50px;
-    border: 2px solid white;
-    border-radius: 9em;
-    left: 1px;
-    top: 1px;
-  }
-`;
-
-const SocialButtonLabel = styled.span`
-  max-width: 100%;
-  display: inline-block;
-  font-size: 0.9em;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  padding-top: 0.5em;
-`;
-
 const HelpText = styled.p`
   border: 1px solid #dbdbdb;
   border-width: 1px 0;
@@ -222,13 +117,6 @@ const HelpText = styled.p`
   text-align: center;
   font-size: 0.9em;
   margin: 0.75em 0 1em;
-`;
-
-const InstagramMosaic = styled.div`
-  display: grid;
-  width: 100%;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 3px;
 `;
 
 const YoutubeMosaic = styled.div`
